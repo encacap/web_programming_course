cc466f1c1b1d87e768ee6c8c3ff1cf091c2e13dd
Nguyen Khac Khanh <encacap@gmail.com>
2021-09-20 15:24:15 +0700

    Complete get_post assignments

---
+++ get_post/get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET - 1851120020 - Nguyễn Khắc Khánh</title>
+</head>
+<body>
+    <h1>GET</h1>
+    <?php 
+        echo "<h3>Name: ".$_GET["name"]."</h1>";
+        echo "<h3>Age: ".$_GET["age"]."</h1>";
+    ?>
+</body>
+</html>
---
+++ get_post/post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST - 1851120020 - Nguyễn Khắc Khánh</title>
+</head>
+<body>
+    <h1>POST</h1>
+    <?php 
+        echo "<h3>Name: ".$_POST['name']."</h1>";
+        echo "<h3>Age: ".$_POST['age']."</h1>";
+    ?>
+</body>
+</html>
